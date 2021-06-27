a2f16d0e52fda49de1b41edb0fa73c1ce903e1bc
hjy <2237995998@qq.com>
2021-06-27 12:40:52 +0800

    修复部署之后页面刷新出现404的bug

--- back/education-back/src/main/java/com/ye/back/BackApplication.java
+++ back/education-back/src/main/java/com/ye/back/BackApplication.java
@@ -3,6 +3,11 @@ package com.ye.back;
 import com.github.jeffreyning.mybatisplus.conf.EnableMPP;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.server.ConfigurableWebServerFactory;
+import org.springframework.boot.web.server.ErrorPage;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
 
 @SpringBootApplication(scanBasePackages = "com.ye.back")
 @EnableMPP
@@ -12,4 +17,15 @@ public class BackApplication {
         SpringApplication.run(BackApplication.class, args);
     }
 
+    /**
+     * 解决部署之后刷新页面404问题
+     * @return
+     */
+    @Bean
+    public WebServerFactoryCustomizer<ConfigurableWebServerFactory> webServerFactoryCustomizer(){
+        return factory -> {
+            ErrorPage error404Page = new ErrorPage(HttpStatus.NOT_FOUND, "/");
+            factory.addErrorPages(error404Page);
+        };
+    }
 }
